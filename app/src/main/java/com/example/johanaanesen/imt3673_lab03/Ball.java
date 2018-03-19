754f792dc154af4e242304423652491dea3636b6
Johan Aanesen <johanaan@stud.ntnu.no>
2018-03-19 19:05:38 +0100

    merp almost there

---
+++ app/src/main/java/com/example/johanaanesen/imt3673_lab03/Ball.java
@@ -0,0 +1,24 @@
+package com.example.johanaanesen.imt3673_lab03;
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class Ball {
+    public ImageView ball;
+
+    public float posX, posY, accX, accY, velX, velY, maxPosX, maxPosY;
+
+    public Ball(View Ball, float maxX, float maxY){
+        this.maxPosX = maxX;
+        this.maxPosY = maxY;
+
+        this.accX = 0; this.accY = 0;
+        this.velX = 0; this.velY = 0;
+        this.posX = this.maxPosX / 2; this.posY = this.maxPosY / 2;
+
+
+        this.ball = (ImageView) Ball;
+        this.ball.setX(this.posX);
+        this.ball.setY(this.posY);
+    }
+}
--- app/src/main/java/com/example/johanaanesen/imt3673_lab03/MainActivity.java
+++ app/src/main/java/com/example/johanaanesen/imt3673_lab03/MainActivity.java
@@ -1,13 +1,144 @@
 package com.example.johanaanesen.imt3673_lab03;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Resources;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Display;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+    private Ball ball;
+
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    private Vibrator vibrator;
+
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
+
+        init();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        this.sensorManager.registerListener(this, this.sensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        this.sensorManager.unregisterListener(this);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        this.ball.accX = -(sensorEvent.values[0] / 7);
+        this.ball.accY = sensorEvent.values[1] / 7;
+
+        this.ball.velX = this.ball.velX + this.ball.accX;
+        this.ball.velY = this.ball.velY + this.ball.accY;
+
+        if(this.ball.velX > 15){
+            this.ball.velX = 15;
+        }
+        if(this.ball.velX < -15){
+            this.ball.velX = -15;
+        }
+        if(this.ball.velY > 15){
+            this.ball.velY = 15;
+        }
+        if(this.ball.velY < -15){
+            this.ball.velY = -15;
+        }
+
+        this.ball.posX = this.ball.posX + this.ball.velX;
+        this.ball.posY = this.ball.posY + this.ball.velY;
+
+        if(this.ball.posX < 0) {
+            this.ball.posX = 0;
+            this.ball.velX = -this.ball.velX * 0.8f;
+            collision();
+        }
+
+        if (this.ball.posX > this.ball.maxPosX) {
+            this.ball.posX = this.ball.maxPosX;
+            this.ball.velX = -this.ball.velX * 0.8f;
+            collision();
+        }
+
+        if(this.ball.posY < 0) {
+            this.ball.posY = 0;
+            this.ball.velY = -this.ball.velY * 0.8f;
+            collision();
+        }
+
+        if (this.ball.posY > this.ball.maxPosY) {
+            this.ball.posY = this.ball.maxPosY;
+            this.ball.velY = -this.ball.velY * 0.8f;
+            collision();
+        }
+
+        this.ball.ball.setX(this.ball.posX);
+        this.ball.ball.setY(this.ball.posY);
+    }
+
+        @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    public void init() {
+
+        FrameLayout frameLayoutBalance = (FrameLayout)findViewById(R.id.framey);
+        frameLayoutBalance.setBackgroundColor(getResources().getColor(R.color.black));
+
+        Point size = new Point();
+        Display display = getWindowManager().getDefaultDisplay();
+        display.getSize(size);
+
+        int offset = dpToPx(25);
+
+        this.ball = new Ball(findViewById(R.id.ball), size.x - offset, size.y - offset);
+
+
+
+
+        this.sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        this.sensor = this.sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        this.vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        //this.mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.bop);
+    }
+
+    public void collision() {
+        this.vibrator.vibrate(350);
+        //this.mediaPlayer.start();
+    }
+
+    public static int dpToPx(int dp)
+    {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
 }
